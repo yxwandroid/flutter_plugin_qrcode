647e6f6211ba565e4395a2a4ba2915fc357237bd
wilson <yxw_android@163.com>
2019-06-29 18:08:50 +0800

    update

--- lib/flutter_plugin_qrcode.dart
+++ lib/flutter_plugin_qrcode.dart
@@ -6,16 +6,8 @@ class FlutterPluginQrcode {
   static const MethodChannel _channel =
       const MethodChannel('flutter_plugin_qrcode');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
-
-
   static Future<String> get getQRCode async {
     final String version = await _channel.invokeMethod('getQRCode');
     return version;
   }
-
 }
